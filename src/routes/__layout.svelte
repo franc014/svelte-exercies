dbb1c3a40f0e29cbbae7bd03589b6a113a5b1df1
JUan Andrade <jfandradea@gmail.com>
2021-09-04 12:16:23 -0500

    setup site

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/etch-a-sketch">Etch a sketch</a>
+</nav>
+
+<div class="container">
+	<slot />
+</div>
+
+<style>
+	div {
+		width: 200px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/routes/etch-a-sketch.svelte
@@ -0,0 +1,1 @@
+<div>etch</div>
