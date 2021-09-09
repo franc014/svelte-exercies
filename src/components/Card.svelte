e8ba4c50d9025eff7f1f28ab776e5a6471249558
JUan Andrade <jfandradea@gmail.com>
2021-09-09 18:31:25 -0500

    finish exercise

---
+++ src/components/Card.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	export let person;
+	function handleSeeMore() {
+		dispatch('showModal', {
+			person
+		});
+	}
+</script>
+
+<div class="card" data-description={person.description}>
+	<img src={person.photo} alt={person.name} />
+	<h2>{person.name}</h2>
+	<button on:click={handleSeeMore}>Learn more →</button>
+</div>
+
+<style>
+	.card {
+		background: white;
+		padding: 1rem;
+		border-radius: 2px;
+	}
+
+	.card img {
+		width: 100%;
+	}
+
+	.card h2 {
+		color: black;
+	}
+</style>
---
+++ src/components/Person.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let person;
+	$: person = person;
+</script>
+
+{#if person}
+	<img src={person.photo.replace('200', 200)} alt={person.name} width="600" height="600" />
+	<p>{person.description}</p>
+{/if}
+
+<style>
+	img {
+		width: 100%;
+	}
+</style>
