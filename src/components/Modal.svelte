fb93ea511088d899376c864b8119f57d3239cb0e
JUan Andrade <jfandradea@gmail.com>
2021-09-10 16:21:32 -0500

    change showModal prop using component:bind instead afterUpdate

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,28 +1,28 @@
 <script>
-	import { afterUpdate } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 	export let showModal = false;
 
-	afterUpdate(() => {
+	/* afterUpdate(() => {
 		if (!showModal) showModal = false;
-	});
+	}); */
 
 	function closeModal(e) {
 		const isOutside = !e.target.closest('.modal-inner');
-		/* console.log(isOutside);
-		showModal = false; */
 		if (isOutside) {
 			dispatch('closeModal');
 		}
 	}
 </script>
 
-<div class="modal-outer" class:hide-modal={!showModal} on:click={closeModal}>
-	<div class="modal-inner">
-		<slot />
+{#if showModal}
+	<div class="modal-outer" on:click={closeModal}>
+		<div class="modal-inner" out:fade in:fly={{ x: -450, y: -300, duration: 1000 }}>
+			<slot />
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
 	.modal-outer {
@@ -35,19 +35,19 @@
 		left: 0;
 		justify-content: center;
 		align-items: center;
-		/* Hide this modal until we need it */
 	}
-
-	.hide-modal {
+	/* Hide this modal until we need it */
+	/* .hide-modal {
+		
 		opacity: 0;
 		pointer-events: none;
 		transition: opacity 0.2s;
-	}
+	} */
 
-	.modal-outer.open {
+	/* 	.modal-outer.open {
 		opacity: 1;
 		pointer-events: all;
-	}
+	} */
 
 	.modal-inner {
 		max-width: 600px;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <h1>Bienvenido/a</h1>
-<p>Estos son algunos ejemplos realizados con <a href="https://svelte.dev/" >Svelte.js</a></p>
+<p>Estos son algunos ejemplos realizados con <a href="https://svelte.dev/">Svelte.js</a></p>
 <ul>
-    <li><a href="/etch-a-sketch">Etch a sketch</a></li>
+	<li><a href="/etch-a-sketch">Etch a sketch</a></li>
+	<li><a href="/click-outside-modal">Click outside modal</a></li>
 </ul>
